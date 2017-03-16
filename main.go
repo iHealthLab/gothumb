1301c2828050e171c95e3bb8db63afda3d60cf0a
joelchen <joelchen@msn.com>
2017-03-16 17:40:36 +0800

    Update main.go

--- main.go
+++ main.go
@@ -138,7 +138,7 @@ func handleUpload(w http.ResponseWriter, r *http.Request, params httprouter.Para
 	io.WriteString(h, time.Now().String())
 	s := hex.EncodeToString(h.Sum(nil))
 	*key = "files/" + s + "-" + fileNoSpace
-	kind, err := filetype.Match(buf)
+	kind, err := filetype.Match(buffer)
 	if err != nil {
 		fmt.Printf("Error: %s", err)
 		return
