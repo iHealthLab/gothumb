50e6aef3e1e171f162d224b2602837dcd1728a1f
joelchen <joelchen@msn.com>
2017-03-16 18:54:31 +0800

    Update main.go

--- main.go
+++ main.go
@@ -114,9 +114,6 @@ func handleUpload(w http.ResponseWriter, r *http.Request, params httprouter.Para
 		return
 	}
 	fmt.Println("File size: ", fileSize)
-	bytes := make([]byte, fileSize)
-	buffer := bufio.NewReader(file)
-	_, err = buffer.Read(bytes)
 	
 	config := &aws.Config{
 		Region: aws.String(viper.GetString("s3.region")),
@@ -139,8 +136,10 @@ func handleUpload(w http.ResponseWriter, r *http.Request, params httprouter.Para
 	io.WriteString(h, time.Now().String())
 	s := hex.EncodeToString(h.Sum(nil))
 	*key = "files/" + s + "-" + fileNoSpace
-	buf, _ := ioutil.ReadAll(file)
-	kind, err := filetype.Match(buf)
+	bytes := make([]byte, fileSize)
+	buffer := bufio.NewReader(file)
+	_, err = buffer.Read(bytes)
+	kind, err := filetype.Match(bytes)
 	if err != nil {
 		log.Println("Unknown type", err)
 		return
